03c15939016460efe11365c4099b2579e3bbbad1
Marc Pont <marc.pont@presentconnection.eu>
2023-11-06 12:49:29 +0100

    Create startGame component

---
+++ src/components/StartGame.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { isGameStartedGetter, resetPasswordAction } from '../store.ts';
+  const startGame = () => resetPasswordAction();
+</script>
+{#if !$isGameStartedGetter}
+    <button
+        on:click={startGame}
+        id="start"
+        class="bg-green-800 hover:bg-green-600 py-3 px-6 m-12 font-bold uppercase text-white"
+    >
+        Start
+    </button>
+{/if}
